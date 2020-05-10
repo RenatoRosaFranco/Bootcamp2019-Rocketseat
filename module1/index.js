afd857bdb888610b63f1fd75df70be9bb2e34c77
Renato Rosa Franco <renato_ny@live.com>
2020-05-10 18:55:05 -0300

    Bootcamp initialized

---
+++ module1/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const server = express();
+
+// Query params = ?teste=1 => req.query
+// Route params = /users/1 => req.params
+// Request Body = { name: 'Renato Franco', email: 'renato_ny@live.com' }
+
+server.get('/teste', function(req, res){
+    const { nome } = req.query;
+    res.json({ message: `Hello world ${nome}` })
+})
+
+server.get('/users/:id', function(req, res) {
+    const { id } = req.params
+    res.json({ message: `Buscado user ${id}` })
+});
+
+server.listen(4000)
